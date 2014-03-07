57fd69799071dd3daef78c5666826bc413f1fd7e
wayaya <dongsibei2002@163.com>
2014-03-07 14:21:00 +0800

    删除垃圾
    删除了大量的垃圾

--- projects/wayaya@sourceforge/trunk/studydemo/fangchanmanager/target/classes/readme.txt
+++
@@ -1,2 +0,0 @@
-~{PhR*~}vrml~{d/@@Fw2e<~~}
-http://www.kuaipan.cn/file/id_3436875780155823.htm
--- projects/wayaya@sourceforge/trunk/studydemo/sendmail/src/main/java/com/sogou/lyj/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.sogou.lyj;
-
-public class HelloWorld {
-	public static void main(String args[]){
-		System.out.println("dddd");
-	}
-
-}
